3f7bca94b938cc0e83d41d128ffe19069fe4e31b
louis <louiswu0327@gmail.com>
2019-09-19 23:43:40 +0800

    CRUD finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :foods do
+   member do
+     get :search
+   end
+ end
+ root 'foods#search'
 end
