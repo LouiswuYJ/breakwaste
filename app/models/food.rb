3aa6cb01c779c51500fa26958c3cc8d0ab708569
louis <louiswu0327@gmail.com>
2019-09-20 18:21:04 +0800

    Set up user/cart/food/cart_food relation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+
+  has_many :cart_foods
+  has_many :carts, through: :cart_foods
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_090451) do
+ActiveRecord::Schema.define(version: 2019_09_20_101547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_foods", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_foods_on_cart_id"
+    t.index ["user_id"], name: "index_cart_foods_on_user_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "title"
@@ -41,4 +57,7 @@ ActiveRecord::Schema.define(version: 2019_09_20_090451) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_foods", "carts"
+  add_foreign_key "cart_foods", "users"
+  add_foreign_key "carts", "users"
 end
