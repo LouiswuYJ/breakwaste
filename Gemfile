8bd9f3d964c636b59a3e4f38134dced631b71646
louis <louiswu0327@gmail.com>
2019-09-25 14:38:48 +0800

    Add Faker but still processing

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'hirb-unicode', '~> 0.0.5'
+  gem 'faker', '~> 2.4'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "產出10筆PO文資料"
+
+current_user = User.first
+
+5.times do |i|
+  current_user.foods.create(title:Faker::Food.dish,
+                            address:Faker::Address.full_address,
+                            phone:Faker::PhoneNumber.cell_phone,
+                            quantity:Faker::Number.between(from: 1, to: 10),
+                            origin_price:Faker::Number.between(from: 10, to: 500),
+                            discount_price:Faker::Number.between(from: 0.1, to: 0.9),
+                            pickup_time:Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all),
+                            endup_time:Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all),
+                            picture:Faker::LoremPixel.image(size: "200x200", is_gray: false, category: 'food'),
+                            description:Faker::Food.description)
+end
+puts "完成!"
+
