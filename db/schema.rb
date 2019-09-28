e5d293bef803b6c8cc78d47efb9e437296f7b9e2
wutsanyu <b97101053@gmail.com>
2019-09-28 13:20:49 +0800

    add_function destroy current_cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-# ActiveRecord::Schema.define(version: 2019_09_26_042922) do
-# =======
-ActiveRecord::Schema.define(version: 2019_09_25_100016) do
->>>>>>> new_post
+ActiveRecord::Schema.define(version: 2019_09_26_042922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
