40558d34d4a6c76cfed7a45d758e745afb2d0014
fahn <wfliu.1005@gmail.com>
2019-10-05 17:16:52 +0800

    looks fine but always get the payment failed msg

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,6 +20,16 @@ class OrdersController < ApplicationController
   end
 
   def payment
+    @token = braintree_gateway.client_token.generate
+  end
+
+  def braintree_gateway
+    Braintree::Gateway.new(
+      :environment => :sandbox,
+      :merchant_id => Figaro.env.BRAINTREE_MERCHANT_ID,
+      :public_key => Figaro.env.BRAINTREE_PUBLIC_KEY,
+      :private_key => Figaro.env.BRAINTREE_PRIVATE_KEY,
+    )
   end
 
   def giver_order
@@ -55,13 +65,30 @@ class OrdersController < ApplicationController
   end
 
   def transaction
+    if @order.may_pay?
+      result = braintree_gateway.transaction.sale(
+        :amount => "@order.total_price", 
+        :payment_method_nonce => params[:payment_method_nonce],
+        :options => {
+        :submit_for_settlement => true
+        }
+      )
+      if result.success?
+        @order.pay!
+        redirect_to orders_path, notice: '信用卡結帳完成'
+      else
+        redirect_to orders_path, notice: '付款失敗'
+      end
+    else
+      redirect_to orders_path, notice: '訂單已完成付款'
     # if @order.may_pay? #may_pay? 是AASM 產生的方法
     #     @order.pay!       #pay! 是AASM 產生的方法 
     # else
     #   redirect_to orders_path, notice: '訂單已完成付款'
     # end  
+    end
   end
- 
+
   private
   def find_order
     @order = current_user.rescuer_orders.friendly.find(params[:id]) 
