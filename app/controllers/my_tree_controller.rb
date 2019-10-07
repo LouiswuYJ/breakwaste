f8c0625a4785d3643c4a4765b13bc5961a55ea1d
wutsanyu <b97101053@gmail.com>
2019-10-07 18:54:15 +0800

    not finish yet

--- app/controllers/my_tree_controller.rb
+++ app/controllers/my_tree_controller.rb
@@ -2,7 +2,12 @@ class MyTreeController < ApplicationController
   before_action :order_count, only: [:show]
   # before_action :save_money, only: [:show]
   def show
-    
+    rescuer_money = current_user.rescuer_orders.each do |order|
+      order.order_items.each do |item|
+        item.food.origin_price
+      end
+    end
+    byebug
   end
   private
   def order_count
@@ -10,6 +15,18 @@ class MyTreeController < ApplicationController
     rescuer_orders = current_user.rescuer_orders.count #計算Rescuer訂單總數
     @order_count = giver_orders + rescuer_orders
   end
+
+  # def find_rescuer_orders_items
+  #   current_user.rescuer_orders.each do |order|
+  #     order.order_items
+  #   end
+  # end
+
+  # def find_giver_orders_items
+  #   current_user.giver_orders.each do |order|
+  #     order.order_items 
+  #   end
+  # end
 
   # def save_money
   #   giver_money = current_user.giver_orders.order_items.foods.each do |oprice, dprice|
