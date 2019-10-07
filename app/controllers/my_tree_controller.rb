923701c762bbf9cc35cf4a788e1e63d19c85f35c
wutsanyu <b97101053@gmail.com>
2019-10-07 15:58:57 +0800

    change tree photos

--- app/controllers/my_tree_controller.rb
+++ app/controllers/my_tree_controller.rb
@@ -1,11 +1,26 @@
 class MyTreeController < ApplicationController
-  
+  before_action :order_count, only: [:show]
+  # before_action :save_money, only: [:show]
   def show
-    giver_orders = current_user.giver_orders.count
-    rescuer_orders = current_user.rescuer_orders.count
+    
+  end
+  private
+  def order_count
+    giver_orders = current_user.giver_orders.count #計算Giver訂單總數
+    rescuer_orders = current_user.rescuer_orders.count #計算Rescuer訂單總數
     @order_count = giver_orders + rescuer_orders
   end
 
+  # def save_money
+  #   giver_money = current_user.giver_orders.order_items.foods.each do |oprice, dprice|
+  #     oprice - dprice
+  #   end
+
+  #   recuser_money = current_user.rescuer_orders.order_items.foods.each do |oprice, dprice|
+  #     oprice - dprice
+  #   end
+  #   @save_money = giver_money + recuser_money
+  # end
 end
 
 
