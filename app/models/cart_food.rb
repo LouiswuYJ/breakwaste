4008f877254e4cbdfbe598a1fed49f8fef9c5755
louis <louiswu0327@gmail.com>
2019-10-07 14:50:55 +0800

    Add cart total price / endup_time only show hour:mins

--- app/models/cart_food.rb
+++ app/models/cart_food.rb
@@ -3,4 +3,10 @@ class CartFood < ApplicationRecord
   belongs_to :food
 
   belongs_to :giver, class_name: 'User', foreign_key: :giver_id    #預設的外部鍵叫做 giver_id
+
+  def total_price
+    food = CartFood.find_by(giver_id: giver.id) 
+    price = Food.find(food.food_id).discount_price
+    food.quantity * price
+  end
 end
