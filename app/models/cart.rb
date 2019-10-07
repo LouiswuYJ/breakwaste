e0fd0a88dfdfd0a83b7573a46f77965828973aa5
louis <louiswu0327@gmail.com>
2019-10-07 14:50:55 +0800

    Add cart total price / endup_time only show hour:mins

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,8 @@ class Cart < ApplicationRecord
 
   has_many :cart_foods, dependent: :destroy
   has_many :foods, through: :cart_foods
+
+  def total_price
+    cart_foods.reduce(0) { |sum, item| sum + item.total_price }
+  end
 end
