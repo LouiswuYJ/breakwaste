0ff3de92c6e19ff1a92ff24c001e2c858def5dd2
wutsanyu <b97101053@gmail.com>
2019-10-07 15:08:59 +0800

    add my_tree_helper.rb

--- app/controllers/my_tree_controller.rb
+++ app/controllers/my_tree_controller.rb
@@ -1,7 +1,11 @@
 class MyTreeController < ApplicationController
   
   def show
-    
+    giver_orders = current_user.giver_orders.count
+    rescuer_orders = current_user.rescuer_orders.count
+    @order_count = giver_orders + rescuer_orders
   end
 
 end
+
+
--- app/helpers/my_tree_helper.rb
+++ app/helpers/my_tree_helper.rb
@@ -1,3 +1,11 @@
 module MyTreeHelper
-
+  def order_count(order_count)    
+    if order_count <= 6
+      image_tag "trees/tree1.jpg"
+    elsif order_count > 6 && order_count <= 30
+      image_tag "trees/tree2.jpg"
+    else order_count >30
+      image_tag "trees/tree3.jpg"
+    end 
+  end
 end
