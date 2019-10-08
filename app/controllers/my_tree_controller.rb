2d0624e1edda115309559081381008ada3e1a97c
wutsanyu <b97101053@gmail.com>
2019-10-08 12:33:05 +0800

    add recuser_money

--- app/controllers/my_tree_controller.rb
+++ app/controllers/my_tree_controller.rb
@@ -1,19 +1,31 @@
 class MyTreeController < ApplicationController
   before_action :order_count, only: [:show]
-  # before_action :save_money, only: [:show]
+  before_action :recuser_money, only: [:show]
+
   def show
-    rescuer_money = current_user.rescuer_orders.each do |order|
-      order.order_items.map do |item|
-        item.food.origin_price - item.food.discount_price
-      end
-    end
+    @save_orders = Order.all.count #網站總訂單數
   end
+
   private
-  def order_count
+  def order_count #計算訂單總數
     giver_orders = current_user.giver_orders.count #計算Giver訂單總數
     rescuer_orders = current_user.rescuer_orders.count #計算Rescuer訂單總數
     @order_count = giver_orders + rescuer_orders
   end
+
+  def recuser_money #計算省下的錢
+    rescuer_items = current_user.rescuer_orders.map do |order|
+      order.order_items.map do |item| 
+        item.food.origin_price - item.food.discount_price
+      end
+    end
+    #把登入者的rescuer_orders一項一項抽出來，分項計算，order_item中每一項食物原價減去折扣價
+    @rescuer_money = rescuer_items.flatten.sum 
+    #rescuer_items計算結果會是雙重陣列，先抽出來再計算總金額    
+  end
+
+
+
 
   # def find_rescuer_orders_items
   #   current_user.rescuer_orders.each do |order|
